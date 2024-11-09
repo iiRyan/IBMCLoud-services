48633e53c5c27ad35cfab4ca9ba54036dd994b67
iiRyan <iirharbi@gmail.com>
2024-11-09 14:19:58 +0300

    List all object method

--- src/main/java/com/ibmcloud/rest/cloud/service/cos/BucketResource.java
+++ src/main/java/com/ibmcloud/rest/cloud/service/cos/BucketResource.java
@@ -1,9 +1,8 @@
 package com.ibmcloud.rest.cloud.service.cos;
 
-import java.util.List;
 
 public interface BucketResource {
 
     	String findAllOpBuckets();
-
+        void listObjects(String bucketName);
 } 
--- src/main/java/com/ibmcloud/rest/cloud/service/cos/BucketResourceImpl.java
+++ src/main/java/com/ibmcloud/rest/cloud/service/cos/BucketResourceImpl.java
@@ -6,17 +6,37 @@ import java.util.stream.Collectors;
 
 import com.ibm.cloud.objectstorage.services.s3.AmazonS3;
 import com.ibm.cloud.objectstorage.services.s3.model.Bucket;
+import com.ibm.cloud.objectstorage.services.s3.model.ListObjectsRequest;
+import com.ibm.cloud.objectstorage.services.s3.model.ObjectListing;
+import com.ibm.cloud.objectstorage.services.s3.model.S3ObjectSummary;
 
 public class BucketResourceImpl implements BucketResource {
 
+    private static final AmazonS3 cosClient = COSClient.INSTANCE.getCosClient();
+
     @Override
     public String findAllOpBuckets() {
-         AmazonS3 cos = COSClient.INSTANCE.getCosClient();
-        List<Bucket> buckets = cos.listBuckets();
+        List<Bucket> buckets = cosClient.listBuckets();
 
         return buckets.stream()
-               .filter(Objects::nonNull)
-               .map(Bucket::getName)
-               .collect(Collectors.joining(", "));
+                .filter(Objects::nonNull)
+                .map(Bucket::getName)
+                .collect(Collectors.joining(", "));
+    }
+    
+    @Override
+    public void listObjects(String bucketName) {
+        System.out.println("Listing objects in bucket " + bucketName);
+        ObjectListing objectListing = cosClient.listObjects(new ListObjectsRequest().withBucketName(bucketName));
+        for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+            System.out.println(" - " + objectSummary.getKey() + "  " + "(size = " + objectSummary.getSize() + ")");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        BucketResourceImpl _cos = new BucketResourceImpl();
+
+        _cos.listObjects("jax-rsbucket");
     }
 }
